864bce8dda5304639ca3d17be0bb12c1179e80aa
Tam Nhu Tran <kaitran.ntt@gmail.com>
2022-05-20 15:26:00 -0400

    Implement a CountDownTimer to see the time

--- app/src/main/java/com/example/test_workflow/MainActivity.kt
+++ app/src/main/java/com/example/test_workflow/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.test_workflow
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Looper
 import android.view.MotionEvent
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.text.DecimalFormat
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     // Declaring handler, runnable and time in milli seconds
@@ -13,12 +18,33 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mRunnable: Runnable
     private var mTime: Long = 10000
 
+    private lateinit var mCounter: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val launchIntent =
             packageManager.getLaunchIntentForPackage("com.example.orangeadapp")
+
+        mCounter = findViewById(R.id.counter)
+
+        object : CountDownTimer(mTime, 1000) {
+            @SuppressLint("SetTextI18n")
+            override fun onTick(millisUntilFinished: Long) {
+                // Used for formatting digit to be in 2 digits only
+                val f: NumberFormat = DecimalFormat("00")
+                val min = millisUntilFinished / 60000 % 60
+                val sec = millisUntilFinished / 1000 % 60
+                mCounter.text =f.format(min) + ":" + f.format(sec)
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onFinish() {
+                mCounter.text = "00:00"
+            }
+
+        }.start()
 
         // Initializing the handler and the runnable
         mHandler = Handler(Looper.getMainLooper())
@@ -50,12 +76,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     // start handler function
-    private fun startHandler(){
+    private fun startHandler() {
         mHandler.postDelayed(mRunnable, mTime)
     }
 
     // stop handler function
-    private fun stopHandler(){
+    private fun stopHandler() {
         mHandler.removeCallbacks(mRunnable)
     }
 
